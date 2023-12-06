616b8b76702aa5760971c3da33dc2241685bd586
Hansebin <sebinn052@gmail.com>
2023-12-06 10:49:39 +0900

    style : styled-components -> tailwindCSS 코드로 변경

--- src/components/spinnerComponent/Spinner.tsx
+++ src/components/spinnerComponent/Spinner.tsx
@@ -1,26 +1,11 @@
 import React from "react";
-import styled from "styled-components";
 import { FadeLoader } from "react-spinners";
-
-const SpinnerContainer = styled.div`
-  width: 100vw;
-  height: 100vh;
-
-  background-color: #e3defe;
-
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-
-  z-index: 9999;
-`;
 
 const Spinner: React.FC = () => {
   return (
-    <SpinnerContainer>
+    <div className="w-screen h-screen flex flex-row justify-center items-center z-[9999] bg-[#e3defe]">
       <FadeLoader color="#7966E4" />
-    </SpinnerContainer>
+    </div>
   );
 };
 
